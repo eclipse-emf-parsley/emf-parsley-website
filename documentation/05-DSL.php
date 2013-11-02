1864e3d4c160a4a6f838f29591b20a07e5bef7bc
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2013-11-02 19:08:41 +0100

    updated documentation

--- documentation/05-DSL.php
+++ documentation/05-DSL.php
@@ -1,6 +1,6 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	include("include.php");
-	$pageTitle 		= "EMF Components DSL";
+	$pageTitle 		= "EMF Parsley DSL";
 	
 	$html = file_get_contents('05-DSL.html');
 
