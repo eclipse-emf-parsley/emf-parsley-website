39d1b61152cfbd326cc95eaddf6e1b09a9e5489e
Francesco Guidieri <francesco.guidieri@gmail.com>
2015-01-30 17:53:57 +0100

    Redirect to documentation added
    Conflicts:
    	documentation/01-Introduction.php
    
    Signed-off-by: Francesco Guidieri <francesco.guidieri@gmail.com>

--- documentation/01-Introduction.php
+++ documentation/01-Introduction.php
@@ -1,5 +1,1 @@
-<<<<<<< Upstream, based on origin/master
-<?php header('Location: https://www.eclipse.org/emf-parsley/documentation.html'); ?>
-=======
 <?php header("Location: https://www.eclipse.org/emf-parsley/documentation.html"); ?>
->>>>>>> 6dc1c32 Redirect to documentation added
